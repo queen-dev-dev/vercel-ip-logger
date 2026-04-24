18cc556d9f119465b916856c34f4d978bceaacba
Devon <100230169@student.burnley.ac.uk>
2026-04-24 11:43:48 +0100

    initial commit

---
+++ api/IPLogger.ts
@@ -0,0 +1,11 @@
+import { ipAddress } from '@vercel/functions'
+
+export default {
+    fetch(request) {
+        const ip = ipAddress(request);
+        console.log(ip)
+        return {
+            ip: "silly"
+        }
+    }
+}
